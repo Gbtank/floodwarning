3e4f7fd99bb8cbb6365e2e3e6f59bc8466058c58
Gbtank <bednarkiewiczg@gmail.com>
2022-01-30 12:46:15 +0000

    Forgot to stage geo.py commits, adding now.

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,26 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
+def rivers_with_station(stations):
+    """Takes a list of stations and
+    returns all unique rivers associated
+    with the stations.
+    """
+    rivers = set()
+    for station in stations:
+        rivers.add(station.river)
+    return rivers
+
+def stations_by_river(stations):
+    """Takes a list of MonitoringStation
+    objects and maps each station's name (value) to its
+    respective river (key).
+    """
+    station_map = {}
+    for station in stations:
+        if station.river in station_map:
+            station_map[station.river].add(station.name)
+        else:
+            station_map[station.river] = { station.name }
+    
+    return station_map
